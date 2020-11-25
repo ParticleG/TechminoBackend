2317a4d141099f977fd9f8c6962a1bb85985ff99
Particle_G <gzy1135989508@gmail.com>
2020-11-25 21:26:05 +0800

    Initial socket methods.

--- controllers/tech_api_v1.h
+++ controllers/tech_api_v1.h
@@ -5,6 +5,17 @@
 
 using namespace drogon;
 namespace tech::api::v1 {
+    struct JsonResponse {
+        HttpStatusCode code;
+        Json::Value body;
+
+        void send(std::function<void(const HttpResponsePtr &)> callback) {
+            auto httpJsonResponse = HttpResponse::newHttpJsonResponse(body);
+            httpJsonResponse->setStatusCode(code);
+            callback(httpJsonResponse);
+        }
+    };
+
     class App : public drogon::HttpController<App> {
     public:
         METHOD_LIST_BEGIN
@@ -29,29 +40,38 @@ namespace tech::api::v1 {
             //METHOD_ADD(Users::get,"/{2}/{1}",Get);//path is /tech/api/v1/Users/{arg2}/{arg1}
             //METHOD_ADD(Users::your_method_name,"/{1}/{2}/list",Get);//path is /tech/api/v1/Users/{arg1}/{arg2}/list
             //ADD_METHOD_TO(Users::your_method_name,"/absolute/path/{1}/{2}/list",Get);//path is /absolute/path/{arg1}/{arg2}/list
-            METHOD_ADD(Users::registration, "", Post);
+            METHOD_ADD(Users::create, "", Post);
             METHOD_ADD(Users::login, "", Get);
+            METHOD_ADD(Users::modify, "", Put);
+
+            METHOD_ADD(Users::getData, "/{1}", Get);
+            METHOD_ADD(Users::saveData, "/{1}", Put);
 
         METHOD_LIST_END
 
         // your declaration of processing function maybe like this:
         // void get(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,int p1,std::string p2);
         // void your_method_name(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,double p1,int p2) const;
-        static void registration(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+        static void create(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+
         static void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 
+        static void modify(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+
+        static void getData(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
+                            std::string userId);
+
+        static void saveData(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
+                             std::string userId);
+
     private:
-        static std::string sha256(const std::string &srcStr) {
-            unsigned char mdStr[33] = {0};
-            SHA256((const unsigned char *) srcStr.c_str(), srcStr.length(), mdStr);
-            char buf[65] = {0};
-            char tmp[3] = {0};
-            for (int i = 0; i < 32; i++) {
-                sprintf_s(tmp, "%02x", mdStr[i]);
-                strcat_s(buf, tmp);
-            }
-            buf[32] = '\0';
-            return std::string(buf);
-        }
+        static std::string _sha256(const std::string &srcStr);
+
+        static void
+        _refreshToken(JsonResponse &jsonResponse, int id, const std::string &auth_token);
+
+        static void
+        _updateToken(JsonResponse &jsonResponse, std::string email, const std::string &password);
+
     };
 }
