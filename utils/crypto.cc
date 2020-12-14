06525ff4895af20c4c6cc4e68d4871eadfe8a65c
Particle_G <1135989508@qq.com>
2020-12-15 00:03:53 +0800

    Update crypto.cc

--- utils/crypto.cc
+++ utils/crypto.cc
@@ -9,26 +9,19 @@ using namespace tech::utils;
 std::string Crypto::md5(const std::string &srcStr) {
     unsigned char mdStr[33] = {0};
     MD5((const unsigned char *) srcStr.c_str(), srcStr.length(), mdStr);
-
-    char buf[65] = {0};
-    char tmp[3] = {0};
+    std::ostringstream oss;
     for (int i = 0; i < 32; i++) {
-        sprintf_s(tmp, "%02x", mdStr[i]);
-        strcat_s(buf, tmp);
+        oss << std::hex << static_cast<unsigned short>(mdStr[i]);
     }
-    buf[32] = '\0';
-    return std::string(buf);
+    return oss.str();
 }
 
 std::string Crypto::sha256(const std::string &srcStr) {
     unsigned char mdStr[33] = {0};
     SHA256((const unsigned char *) srcStr.c_str(), srcStr.length(), mdStr);
-    char buf[65] = {0};
-    char tmp[3] = {0};
+    std::ostringstream oss;
     for (int i = 0; i < 32; i++) {
-        sprintf_s(tmp, "%02x", mdStr[i]);
-        strcat_s(buf, tmp);
+        oss << std::hex << static_cast<unsigned short>(mdStr[i]);
     }
-    buf[32] = '\0';
-    return std::string(buf);
+    return oss.str();
 }
