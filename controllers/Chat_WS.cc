a96252f783578053c4d1a9e0229b32c48c747ad2
Particle_G <particle_g@outlook.com>
2021-03-03 03:54:49 +0800

    - Using new inheriting methods for Chat WS

--- controllers/Chat_WS.cc
+++ controllers/Chat_WS.cc
@@ -2,49 +2,12 @@
 // Created by Parti on 2021/2/5.
 //
 
-#include "Chat_WS.h"
-#include <utils/WebSocket.h>
+#include <controllers/Chat_WS.h>
+#include <services/Chat_WS.h>
 
-using namespace tech::plugin;
 using namespace tech::socket::v1;
-using namespace tech::utils;
-using namespace drogon;
 using namespace std;
 
-void Chat::handleNewMessage(const WebSocketConnectionPtr &wsConnPtr, string &&message,
-                            const WebSocketMessageType &type) {
-    if (type == WebSocketMessageType::Ping) {
-        LOG_DEBUG << "Received a PING";
-    } else if (type == WebSocketMessageType::Text || type == WebSocketMessageType::Binary) {
-        _service.messageHandler(wsConnPtr, message);
-    } else if (type == WebSocketMessageType::Pong) {
-        LOG_DEBUG << "Message is Pong";
-    } else if (type == WebSocketMessageType::Close) {
-        LOG_DEBUG << "Message is Close";
-    } else if (type == WebSocketMessageType::Unknown) {
-        LOG_DEBUG << "Message is Unknown";
-    }
-}
-
-void Chat::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr &wsConnPtr) {
-    CloseCode code;
-    string reason;
-
-    string email = req->getParameter("email"),
-            accessToken = req->getParameter("token"),
-            config = req->getParameter("conf"),
-            roomID = app().getPlugin<ChatManager>()->getID();
-
-    if ((!_service.validate(wsConnPtr, email, accessToken, config, roomID, code, reason)) ||
-        (!_service.join(wsConnPtr, code, reason))) {
-        wsConnPtr->send(reason);
-        WebSocket::close(wsConnPtr, code, reason);
-        return;
-    }
-}
-
-void Chat::handleConnectionClosed(const WebSocketConnectionPtr &wsConnPtr) {
-    if (wsConnPtr->hasContext()) {
-        _service.quit(wsConnPtr);
-    }
+Chat::Chat() {
+    _service = make_shared<tech::services::websocket::Chat>();
 }
