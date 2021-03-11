ad20781fc80844bcf0f31272cbc132d498ff7963
Particle_G <particle_g@outlook.com>
2021-03-11 12:39:37 +0800

    - Fix controllers' inherits

--- controllers/User_WS.h
+++ controllers/User_WS.h
@@ -5,18 +5,13 @@
 #pragma once
 
 #include <controllers/Base_WS.h>
+#include <services/User_WS.h>
 
 namespace tech::socket::v1 {
-    class User : public drogon::WebSocketController<User>, public Base {
+    class User : public Base<User, tech::services::websocket::User> {
     public:
-        using Base::handleNewMessage;
-        using Base::handleNewConnection;
-        using Base::handleConnectionClosed;
-
         WS_PATH_LIST_BEGIN
             WS_PATH_ADD("/tech/socket/v1/user", "tech::filters::User");
         WS_PATH_LIST_END
-
-        User();
     };
 }
