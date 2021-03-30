fcc5fda85df38a40a3cadde2b2da886d14492743
Particle_G <particle_g@outlook.com>
2021-03-30 13:44:50 +0800

    - Remove redundant semicolon

--- controllers/User_WS.h
+++ controllers/User_WS.h
@@ -11,7 +11,7 @@ namespace tech::socket::v1 {
     class User : public Base<User, tech::services::websocket::User> {
     public:
         WS_PATH_LIST_BEGIN
-            WS_PATH_ADD("/tech/socket/v1/user", "tech::filters::User");
+            WS_PATH_ADD("/tech/socket/v1/user", "tech::filters::User")
         WS_PATH_LIST_END
         User();
     };
